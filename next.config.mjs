53cd25e3696ffb9234cd5a8194a9b0603f2df70e
Shivamkus <kushwahshivam065@.com>
2024-02-23 13:02:42 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+const nextConfig = {
+    images: {
+        domains: ['res.cloudinary.com','randomuser.me'],
+      },
+      // experimental:{
+      //   reactRoot: true,
+      //   suppressHydrationWarning: true,
+      // }
+}
+// module.exports = nextConfig
+
+export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
